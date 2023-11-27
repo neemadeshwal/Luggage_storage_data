8305155e4b45fa172589aba7722f6b0115bc5f40
udaykiran1012 <123919244+udaykiran1012@users.noreply.github.com>
2023-11-27 18:33:38 +0530

    Create a login page

--- pages/Login.jsx
+++ pages/Login.jsx
@@ -1,7 +1,27 @@
-import React from "react";
 
+import profile from './Assets/png-transparent-computer-icons-user-login-swiggy-blue-text-computer-removebg-preview.png'
 export default function Login(){
     return(
-        <h1>Login page</h1>
-    )
+            <div className="container">
+                <div className='logo'>
+                    <img className='logo-img' src={profile} alt=""></img>
+                </div>
+                <h2 className='heading'>Sign In</h2>
+                <form action="/login" method="POST">
+                    <input className='fields'  type="tel" placeholder='Enter your Phone' pattern='[0-9]{10}' required />
+                    <input className='fields' type="password"  placeholder='Enter your password' required />
+                    <button className='btn' type='submit'>Sign In</button>
+                </form>
+                <div className='lower'>
+                    <div className='left'>
+                        <input type="checkbox"/>
+                        <p>Remember me</p>
+                    </div>
+                    <p><a href="#">Forgot Password?</a></p>
+                </div>
+                <div className='footer'>
+                    <p>Not a Member? <a href="#">Create an Account</a></p>
+                </div>
+            </div>
+)
 }
