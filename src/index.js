e7ac0b99991a12bee8d5d33031f992e559354600
mukulmeena186 <mukulmeena186@gmail.com>
2023-11-28 17:37:58 +0530

    create forget password page

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export {default as Home} from "../pages/Home"
 export {default as Register} from "../pages/Register"
 export {default as Login} from "../pages/Login"
+export {default as Forget_pass} from "../pages/Forgetpass"
